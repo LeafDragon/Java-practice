b449e8c33f2b699def08962d79cdbd409914fef9
Frank Dip <33531671+LeafDragon@users.noreply.github.com>
2018-02-01 12:18:25 -0800

    ArrayEg2

---
+++ ArrayEg2.java
@@ -0,0 +1,16 @@
+class ArrayEg2 {
+  public static void main(String[] args) {
+    double[] val = new double[4];
+    
+    val[0] = 0.12;
+    val[1] = 1.43;
+    val[2] = 2.98;
+    
+    int j = 3;
+    System.out.println("cell 3: " + val[j]);
+    System.out.println("cell 2: " + val[j -1]);
+    
+    j -= 2;
+    System.out.println("cell 1: " + val[j]);
+  }
+}
